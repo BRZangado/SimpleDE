a12279ba73bf717155706cbabfc0daaed2fd963b
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2018-04-17 21:18:47 -0300

    update cursos.html

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from simplemooc.courses.models import Course
 
 def index(request):
+	courses = Course.objects.all()
 	template_name = 'cursos/cursos.html'
-	return render(request, template_name)
+	context = {
+		'courses':courses
+	}
+	return render(request, template_name, context)
