88d74d6fc820d685525f69baaa1e02b3816174ba
Bruno Rodrigues Santos <bruesmanbruesman@hotmail.com>
2018-05-03 19:50:51 -0300

    added mail function with html templates (sending email via terminal)

--- simplemooc/courses/forms.py
+++ simplemooc/courses/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
+from simplemooc.core.mail import send_mail_template
 
 class ContactCourse(forms.Form):
 
@@ -10,11 +11,19 @@ class ContactCourse(forms.Form):
 
 	def sendMail(self, course):
 		subject = 'Contato sobre o curso %s ' % course
-		message = 'Nome: %(name)s; \nEmail: %(email)s; \n%(message)s'
 		context = {
 			'name':self.cleaned_data['name'],
 			'email':self.cleaned_data['email'],
 			'message':self.cleaned_data['message']
 		}
-		message = message % context
-		send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.CONTACT_EMAIL])
+
+		template_name = 'cursos/email_template.html'
+
+		send_mail_template(
+
+			subject,
+			template_name,
+			context,
+			[settings.CONTACT_EMAIL]
+
+		)
